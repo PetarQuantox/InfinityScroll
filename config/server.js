f4a1a39eb4a42de7b0a9a24f81dda0b1700dbce3
Petar <petar.pavlovic@quantox.com>
2019-03-26 14:47:40 +0100

    Added client for front end

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports  = {
+  APPLICATION_ID: process.env.APPLICATION_ID || '2e2a3aa711ce65d8efa93e2e1e09ca88d88b2e3bffd3ace5aec0b8d5a6c70ec7',
+  SECRET: process.env.SECRET || '8d9275ba40236518cbb9db74536aa17718c4fbdd81feb9892b338b02259c05d7',
+  CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+}
